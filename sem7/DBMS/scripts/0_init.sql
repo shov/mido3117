348bbd643a3907feccb3287363ee1b051e9a3350
Alexandr Shevchenko <ls.shov@gmail.com>
2021-01-13 04:00:46 +0300

    BNTU-19: DBMS init script first part

--- sem7/DBMS/scripts/0_init.sql
+++ sem7/DBMS/scripts/0_init.sql
@@ -28,100 +28,71 @@ CREATE TABLE IF NOT EXISTS movies (
 CREATE TABLE IF NOT EXISTS movie_stats (
     id       BIGSERIAL PRIMARY KEY,
     movie_id BIGINT REFERENCES movies(id) ON DELETE CASCADE,
-    name     TEXT           NOT NULL,
+    name     TEXT NOT NULL,
     value    NUMERIC(12, 2),
     UNIQUE (movie_id, name)
 );
 
-INSERT INTO movies
-    (genre_id, name, description, year, run_time)
-VALUES ((SELECT id FROM genres WHERE name = 'action'),
-        'Скотт Пилигрим против всех',
-        'Юный рок-музыкант готов сразиться с семью злобными бывшими xxxxxxx',
-        2010,
-        112),
-((SELECT id FROM genres WHERE name = 'drama'),
- 'Побег из Шоушенка',
- 'Выдающаяся драма о силе таланта, важности дружбы, стремлении к свободе и Рите Хэйворт',
- 1994,
- 142),
-((SELECT id FROM genres WHERE name = 'drama'),
- 'Зеленая миля',
- 'В тюрьме для смертников появляется заключенный с божественным даром',
- 1999,
- 189),
-((SELECT id FROM genres WHERE name = 'sci-fi'),
- 'Интерстеллар',
- 'Эпос про задыхающуюся Землю, космические полеты и парадоксы времени',
- 2014,
- 169),
-((SELECT id FROM genres WHERE name = 'comedy'),
- 'Криминальное чтиво',
- 'Фильм Квентина Тарантино',
- 1994,
- 154),
-((SELECT id FROM genres WHERE name = 'comedy'),
- 'Иван Васильевич меняет профессию',
- 'Комедия Леонида Гайдая на все времена',
- 1973,
- 88)
-ON CONFLICT DO NOTHING;
+CREATE OR REPLACE
+    PROCEDURE
+    fill_movie(in_genre_name TEXT, in_name TEXT, in_description TEXT,
+               in_year       INT, in_run_time BIGINT,
+               in_rating     NUMERIC, in_audience NUMERIC,
+               in_sold_world NUMERIC, in_sold_local NUMERIC)
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    movie_ret_id BIGINT;
+BEGIN
+    INSERT INTO movies
+        (genre_id, name, description, year, run_time)
+    VALUES ((SELECT id FROM genres WHERE name = in_genre_name),
+            in_name, in_description, in_year, in_run_time)
+    ON CONFLICT DO NOTHING
+    RETURNING id INTO movie_ret_id;
 
+    IF movie_ret_id IS NULL
+    THEN
+        RETURN;
+    END IF;
 
-INSERT INTO movie_stats (movie_id, name, value)
-VALUES ((SELECT id FROM movies WHERE name = 'Скотт Пилигрим против всех' AND year = 2010),
-        'rating', 9.1),
-((SELECT id FROM movies WHERE name = 'Скотт Пилигрим против всех' AND year = 2010),
- 'audience', 16),
-((SELECT id FROM movies WHERE name = 'Скотт Пилигрим против всех' AND year = 2010),
- 'sold_world', 47664559),
-((SELECT id FROM movies WHERE name = 'Скотт Пилигрим против всех' AND year = 2010),
- 'sold_local', 1095677),
+    INSERT INTO movie_stats (movie_id, name, value)
+    VALUES (movie_ret_id, 'rating', in_rating),
+        (movie_ret_id, 'audience', in_audience),
+        (movie_ret_id, 'sold_world', in_sold_world),
+        (movie_ret_id, 'sold_local', in_sold_local)
+    ON CONFLICT DO NOTHING;
+END
+$$;
 
-((SELECT id FROM movies WHERE name = 'Побег из Шоушенка' AND year = 1994),
- 'rating', 7.2),
-((SELECT id FROM movies WHERE name = 'Побег из Шоушенка' AND year = 1994),
- 'audience', 16),
-((SELECT id FROM movies WHERE name = 'Побег из Шоушенка' AND year = 1994),
- 'sold_world', 28418687),
-((SELECT id FROM movies WHERE name = 'Побег из Шоушенка' AND year = 1994),
- 'sold_local', 28341469),
+CALL fill_movie('action',
+                'Скотт Пилигрим против всех',
+                'Юный рок-музыкант готов сразиться с семью злобными бывшими xxxxxxx',
+                2010, 112, 9.1, 16, 47664559, 1095677);
 
-((SELECT id FROM movies WHERE name = 'Зеленая миля' AND year = 1999),
- 'rating', 9.1),
-((SELECT id FROM movies WHERE name = 'Зеленая миля' AND year = 1999),
- 'audience', 16),
-((SELECT id FROM movies WHERE name = 'Зеленая миля' AND year = 1999),
- 'sold_world', 286801374),
-((SELECT id FROM movies WHERE name = 'Зеленая миля' AND year = 1999),
- 'sold_local', 136801374),
+CALL fill_movie('drama',
+                'Побег из Шоушенка',
+                'Выдающаяся драма о силе таланта, важности дружбы, стремлении к свободе и Рите Хэйворт',
+                1994, 142, 7.2, 16, 28418687, 28341469);
 
-((SELECT id FROM movies WHERE name = 'Интерстеллар' AND year = 2014),
- 'rating', 8.6),
-((SELECT id FROM movies WHERE name = 'Интерстеллар' AND year = 2014),
- 'audience', 12),
-((SELECT id FROM movies WHERE name = 'Интерстеллар' AND year = 2014),
- 'sold_world', 677463813),
-((SELECT id FROM movies WHERE name = 'Интерстеллар' AND year = 2014),
- 'sold_local', 26192066),
+CALL fill_movie('drama',
+                'Зеленая миля',
+                'В тюрьме для смертников появляется заключенный с божественным даром',
+                1999, 189, 9.1, 16, 286801374, 136801374);
 
-((SELECT id FROM movies WHERE name = 'Криминальное чтиво' AND year = 1994),
- 'rating', 8.6),
-((SELECT id FROM movies WHERE name = 'Криминальное чтиво' AND year = 1994),
- 'audience', 18),
-((SELECT id FROM movies WHERE name = 'Криминальное чтиво' AND year = 1994),
- 'sold_world', 213928762),
-((SELECT id FROM movies WHERE name = 'Криминальное чтиво' AND year = 1994),
- 'sold_local', 107928762),
+CALL fill_movie('sci-fi',
+                'Интерстеллар',
+                'Эпос про задыхающуюся Землю, космические полеты и парадоксы времени',
+                2014, 169, 8.6, 12, 677463813, 26192066);
 
-((SELECT id FROM movies WHERE name = 'Иван Васильевич меняет профессию' AND year = 1973),
- 'rating', 8.8),
-((SELECT id FROM movies WHERE name = 'Иван Васильевич меняет профессию' AND year = 1973),
- 'audience', 6),
-((SELECT id FROM movies WHERE name = 'Иван Васильевич меняет профессию' AND year = 1973),
- 'sold_world', NULL),
-((SELECT id FROM movies WHERE name = 'Иван Васильевич меняет профессию' AND year = 1973),
- 'sold_local', NULL)
-ON CONFLICT DO NOTHING;
+CALL fill_movie('comedy',
+                'Криминальное чтиво',
+                'Фильм Квентина Тарантино',
+                1994, 154, 8.6, 18, 213928762, 107928762);
 
+CALL fill_movie('comedy',
+                'Иван Васильевич меняет профессию',
+                'Комедия Леонида Гайдая на все времена',
+                1973, 88, 8.8, 6, NULL, NULL);
 

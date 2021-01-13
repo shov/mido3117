0e27eb3c40b3fa1668351baca8da52b93c221a8d
Alexandr Shevchenko <ls.shov@gmail.com>
2021-01-13 18:04:50 +0300

    BNTU-19: DBMS init script cinemas have been added

--- sem7/DBMS/scripts/0_init.sql
+++ sem7/DBMS/scripts/0_init.sql
@@ -96,3 +96,169 @@ CALL fill_movie('comedy',
                 'Комедия Леонида Гайдая на все времена',
                 1973, 88, 8.8, 6, NULL, NULL);
 
+CREATE TABLE IF NOT EXISTS cinemas (
+    id          BIGSERIAL PRIMARY KEY,
+    name        TEXT NOT NULL UNIQUE,
+    description TEXT,
+    address     TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cinema_halls (
+    id              BIGSERIAL PRIMARY KEY,
+    cinema_id       BIGINT REFERENCES cinemas(id) ON DELETE CASCADE,
+    name            TEXT,
+    acoustic_system TEXT NOT NULL,
+    wide_screen     BOOLEAN DEFAULT (TRUE)
+);
+
+CREATE TABLE IF NOT EXISTS hall_seat_groups (
+    id             BIGSERIAL PRIMARY KEY,
+    cinema_hall_id BIGINT REFERENCES cinema_halls(id) ON DELETE CASCADE,
+    name           TEXT NOT NULL,
+    price_mod      REAL NOT NULL CHECK ( price_mod > 0),
+    amount         INT  NOT NULL CHECK ( amount >= 0 )
+);
+
+DO
+$$
+    BEGIN
+        CREATE TYPE HALL_SEAT_GROUPS_DATA AS (
+            name      TEXT,
+            price_mod REAL,
+            amount    INT
+        );
+    EXCEPTION
+        WHEN DUPLICATE_OBJECT THEN NULL;
+    END
+$$;
+
+DO
+$$
+    BEGIN
+        CREATE TYPE HALL_DATA AS (
+            name            TEXT,
+            acoustic_system TEXT,
+            wide_screen     BOOLEAN,
+            seat_groups     HALL_SEAT_GROUPS_DATA[]
+        );
+    EXCEPTION
+        WHEN DUPLICATE_OBJECT THEN NULL;
+    END
+$$;
+
+CREATE OR REPLACE
+    PROCEDURE fill_cinema(in_c_name TEXT, in_c_description TEXT, in_c_address TEXT,
+                          in_halls  HALL_DATA[])
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    cinema_ret_id       BIGINT;
+    cinema_hall_ret_id  BIGINT;
+
+    hall_row            HALL_DATA;
+    hall_seat_group_row HALL_SEAT_GROUPS_DATA;
+BEGIN
+    INSERT INTO cinemas (name, description, address)
+    VALUES (in_c_name, in_c_description, in_c_address)
+    ON CONFLICT DO NOTHING
+    RETURNING id INTO cinema_ret_id;
+
+    IF cinema_ret_id IS NULL
+    THEN
+        RETURN;
+    END IF;
+
+    FOREACH hall_row IN ARRAY in_halls
+        LOOP
+            INSERT INTO cinema_halls (cinema_id, name, acoustic_system, wide_screen)
+            VALUES (cinema_ret_id,
+                    hall_row.name,
+                    hall_row.acoustic_system,
+                    hall_row.wide_screen)
+            ON CONFLICT DO NOTHING
+            RETURNING id INTO cinema_hall_ret_id;
+
+            IF cinema_hall_ret_id IS NULL
+            THEN
+                CONTINUE;
+            END IF;
+
+            FOREACH hall_seat_group_row IN ARRAY hall_row.seat_groups
+                LOOP
+                    INSERT INTO hall_seat_groups (cinema_hall_id, name, price_mod, amount)
+                    VALUES (cinema_hall_ret_id,
+                            hall_seat_group_row.name,
+                            hall_seat_group_row.price_mod,
+                            hall_seat_group_row.amount)
+                    ON CONFLICT DO NOTHING;
+                END LOOP;
+        END LOOP;
+END
+$$;
+
+CALL fill_cinema(
+        'Сентябрь',
+        'ПН–ВС 10:00—22:00 +375 17 3999426',
+        'г. Минск, пр-т Независимости, 73',
+        ARRAY [
+            ROW ('Кинозал', 'DOLBY 3D', TRUE, ARRAY [
+                ROW ('Партер', 1.0, 600),
+                ROW ('Амфитеатр', 1.1, 500),
+                ROW ('Бельэтаж', 0.8, 80)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                )
+            ]::HALL_DATA[]
+    );
+
+CALL fill_cinema(
+        'Дом Кино',
+        'ПН–ВС 10:00—22:00 +375 17 2723526',
+        'г. Минск, ул. Толбухина, 18',
+        ARRAY [
+            ROW ('Киноконцертный зал', 'Stereo', TRUE, ARRAY [
+                ROW ('Обычное', 1.0, 558),
+                ROW ('VIP', 1.1, 26)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                )
+            ]::HALL_DATA[]
+    );
+
+CALL fill_cinema(
+        'VOKA CINEMA by Silver Screen',
+        '+375 29 3881388 ПН–ЧТ 10:00—24:00 ПТ–СБ 10:00—24:30 ВС 10:00—24:00',
+        'г. Минск, ул. Петра Мстиславца, 11',
+        ARRAY [
+            ROW ('№1', 'Dolby Atmos', TRUE, ARRAY [
+                ROW ('Обычное', 1.0, 99),
+                ROW ('Диван', 1.5, 12)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                ),
+            ROW ('№2', 'Dolby Atmos', TRUE, ARRAY [
+                ROW ('VIP', 1.8, 63)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                ),
+            ROW ('№3', 'Dolby Atmos', TRUE, ARRAY [
+                ROW ('Обычное', 1.0, 198)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                ),
+            ROW ('№4', 'Dolby Atmos', TRUE, ARRAY [
+                ROW ('LUX', 2.0, 48)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                ),
+            ROW ('№5', 'Dolby Atmos', TRUE, ARRAY [
+                ROW ('Обычное', 1.0, 148),
+                ROW ('Диван', 1.5, 12)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                ),
+            ROW ('№6', 'Dolby Atmos', TRUE, ARRAY [
+                ROW ('VIP', 1.8, 68)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                ),
+            ROW ('№7', 'Dolby Atmos', TRUE, ARRAY [
+                ROW ('Обычное', 1.0, 140),
+                ROW ('Диван', 1.5, 12)
+                ]::HALL_SEAT_GROUPS_DATA[]
+                )
+            ]::HALL_DATA[]
+    );

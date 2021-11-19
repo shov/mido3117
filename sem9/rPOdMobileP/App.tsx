1c81846b1fe1d79c7776f142219c5f04376927c7
Alexandr Shevchenko <ls.shov@gmail.com>
2021-11-19 21:59:41 +0100

    BNTU-32: gameloop improved

--- sem9/rPOdMobileP/App.tsx
+++ sem9/rPOdMobileP/App.tsx
@@ -14,6 +14,7 @@ let touches: Array<{
     y: number,
     color: string,
 }> = []
+let lastTouch = [D.width / 2, D.height / 2]
 
 function drawBall(canvas: Canvas, ctx: CanvasRenderingContext2D, x: any, y: any, ballRadius: any, color: string) {
     const arc = new Path2D(canvas)
@@ -27,40 +28,35 @@ function clearCanvas(canvas: Canvas, ctx: CanvasRenderingContext2D) {
 }
 
 function getGameLoop(canvas: Canvas, ctx: CanvasRenderingContext2D, images: CanvasImage[]) {
-    const max = 30
-    let pinch = 0.2
+    const frameRate = 60
+    let prevTime = Date.now()
 
-    return () => {
+    const gameLoop = (time: number) => {
+        const delta = time - prevTime
+        prevTime = time
+        const fps = Math.floor(1000 / delta)
+        let dt = Math.round(delta / (1000 / frameRate)).toFixed(2)
+
+
 
         clearCanvas(canvas, ctx)
 
         images.forEach(i => {
-            ctx.drawImage(i, 100, 100, i.width, i.height)
+            ctx.drawImage(i, lastTouch[0], lastTouch[1], i.width, i.height)
         })
 
-        // @ts-ignore
-        // for (let place of touches) {
-        //     if (place.radius < 1) {
-        //         continue
-        //     }
-        //
-        //     if (place.radius > max) {
-        //         place.multiplier = -1
-        //     }
-        //
-        //     place.radius += pinch * place.multiplier
-        //     if (place.radius < 1) {
-        //         continue
-        //     }
-        //
-        //     drawBall(canvas, ctx, place.x, place.y, place.radius + 4, place.color)
-        // }
+        ctx.fillStyle = '#ffffff'
+        ctx.font = '30px Arial'
+        ctx.fillText(`FPS: ${fps}`, 20, 80)
+        ctx.fillText(`DT: ${dt}`, 20, 120)
+        requestAnimationFrame(time => gameLoop(time))
     }
+    return gameLoop
 }
 
-async function loadImages(canvas: Canvas) {
+async function loadImages(canvas: Canvas): Promise<CanvasImage[]> {
     const i = new CanvasImage(canvas)
-    const asset = Asset.fromModule(require('./assets/Retro-Mario-2-icon.png'));
+    const asset = Asset.fromModule(require('./assets/Retro-Mario-2-icon.png'))
     i.src = asset.uri
 
     return await new Promise(r => {
@@ -78,7 +74,7 @@ async function handleCanvas(canvas: Canvas) {
 
     const images = await loadImages(canvas)
 
-    setInterval(getGameLoop(canvas, ctx, images), 15)
+    requestAnimationFrame(getGameLoop(canvas, ctx, images))
 }
 
 export default function App() {
@@ -95,13 +91,7 @@ export default function App() {
               }
               }
               onTouchMove={(e) => {
-                  // touches.push({
-                  //     radius: 1,
-                  //     multiplier: 1,
-                  //     x: e.nativeEvent.locationX,
-                  //     y: e.nativeEvent.locationY,
-                  //     color: '#ffcf55',
-                  // })
+                  lastTouch = [e.nativeEvent.locationX, e.nativeEvent.locationY]
               }
               }
         >
@@ -115,11 +105,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#000',
+        backgroundColor: '#ff0000',
         flex: 1,
     },
     canvas: {
-        backgroundColor: '#fff',
+        backgroundColor: '#000000',
         position: 'absolute',
         width: '100%',
         height: '100%',

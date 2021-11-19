f3885b35d0309b0145b16d270d2231c93e9df3e5
Alexandr Shevchenko <ls.shov@gmail.com>
2021-11-19 23:35:38 +0100

    BNTU-32: added matter.js physics

--- sem9/rPOdMobileP/App.tsx
+++ sem9/rPOdMobileP/App.tsx
@@ -4,17 +4,12 @@ import {StyleSheet, Text, View, TouchableWithoutFeedback, Dimensions} from 'reac
 import Canvas, {Path2D, CanvasRenderingContext2D, Image as CanvasImage} from 'react-native-canvas'
 import {PanGestureHandler} from "react-native-gesture-handler"
 import {Asset} from 'expo-asset'
+import {Engine, Composite, Bodies, World} from 'matter-js'
 
 const D = Dimensions.get('screen')
 
-let touches: Array<{
-    radius: number,
-    multiplier: number,
-    x: number,
-    y: number,
-    color: string,
-}> = []
 let lastTouch = [D.width / 2, D.height / 2]
+let engine: Engine
 
 function drawBall(canvas: Canvas, ctx: CanvasRenderingContext2D, x: any, y: any, ballRadius: any, color: string) {
     const arc = new Path2D(canvas)
@@ -35,20 +30,18 @@ function getGameLoop(canvas: Canvas, ctx: CanvasRenderingContext2D, images: Canv
         const delta = time - prevTime
         prevTime = time
         const fps = Math.floor(1000 / delta)
-        let dt = Math.round(delta / (1000 / frameRate)).toFixed(2)
+        let dt = Number(Math.round(delta / (1000 / frameRate)).toFixed(2))
 
-
-
+        Engine.update(engine, fps)
         clearCanvas(canvas, ctx)
-
-        images.forEach(i => {
-            ctx.drawImage(i, lastTouch[0], lastTouch[1], i.width, i.height)
-        })
+        const bodiesNumber = render(canvas)
 
         ctx.fillStyle = '#ffffff'
-        ctx.font = '30px Arial'
+        ctx.font = '16px Arial'
         ctx.fillText(`FPS: ${fps}`, 20, 80)
+        ctx.fillText(`Δ: ${delta.toFixed(2)}`, 20, 100)
         ctx.fillText(`DT: ${dt}`, 20, 120)
+        ctx.fillText(`◇: ${bodiesNumber}`, 20, 140)
         requestAnimationFrame(time => gameLoop(time))
     }
     return gameLoop
@@ -66,6 +59,37 @@ async function loadImages(canvas: Canvas): Promise<CanvasImage[]> {
     })
 }
 
+function render(canvas: Canvas) {
+    const ctx = canvas.getContext('2d')
+    let bodies = Composite.allBodies(engine.world)
+
+    const pen = new Path2D(canvas)
+
+    for (let i = 0; i < bodies.length; i += 1) {
+        let vertices = bodies[i].vertices
+
+        if(!bodies[i].isStatic && (vertices[0].y > D.height || vertices[0].x < 0 || vertices[0].x > D.width)) {
+            World.remove(engine.world, bodies[i])
+            console.log('removed body')
+            continue
+        }
+
+        pen.moveTo(vertices[0].x, vertices[0].y)
+
+        for (let j = 1; j < vertices.length; j += 1) {
+            pen.lineTo(vertices[j].x, vertices[j].y)
+        }
+
+        pen.lineTo(vertices[0].x, vertices[0].y)
+    }
+
+    ctx.lineWidth = 1
+    ctx.strokeStyle = '#fff906'
+    ctx.stroke(pen)
+
+    return bodies.length
+}
+
 async function handleCanvas(canvas: Canvas) {
     canvas.width = D.width
     canvas.height = D.height
@@ -74,20 +98,32 @@ async function handleCanvas(canvas: Canvas) {
 
     const images = await loadImages(canvas)
 
+    // matter
+    engine = Engine.create()
+    let boxA = Bodies.rectangle(180, 0, 50, 50)
+    let boxB = Bodies.rectangle(200, 50, 50, 50, {
+
+    })
+    let ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true})
+    Composite.add(engine.world, [boxA, boxB, ground])
+
+    // _matter
+
     requestAnimationFrame(getGameLoop(canvas, ctx, images))
 }
 
 export default function App() {
     return (
         <View style={styles.container}
-              onTouchStart={(e) => {
-                  // touches.push({
-                  //     radius: 1,
-                  //     multiplier: 1,
-                  //     x: e.nativeEvent.locationX,
-                  //     y: e.nativeEvent.locationY,
-                  //     color: '#ff5555',
-                  // })
+              onTouchStart={({nativeEvent}) => {
+                  Composite.add(engine.world, [
+                      Bodies.rectangle(
+                          nativeEvent.locationX,
+                          nativeEvent.locationY,
+                          50,
+                          50,
+                      )
+                  ])
               }
               }
               onTouchMove={(e) => {
@@ -96,9 +132,7 @@ export default function App() {
               }
         >
             <StatusBar hidden={true} />
-            <View style={styles.canvas}>
-                <Canvas style={styles.canvas} ref={handleCanvas} />
-            </View>
+            <Canvas style={styles.canvas} ref={handleCanvas} />
         </View>
     )
 }

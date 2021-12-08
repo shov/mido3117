007c879fdad4e16565a36c5c308efe99f2910d78
Alexandr Shevchenko <ls.shov@gmail.com>
2021-12-08 14:12:04 +0100

    BNTU-33: started with react website

---
+++ sem9/r-web-p/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Square from './components/Square'
+
+function App() {
+  return (
+    <div className="App">
+      <Square/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sem9/r-web-p/src/components/Square.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Square () {
+    return (
+        <div style={{
+            width: 100,
+            height: 100,
+            backgroundColor: '#ff9999',
+        }}/>
+    )
+}
+
+export default Square
